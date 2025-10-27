956191f6fdc6fa270dcda07e15c4aecc9b45ffed
SLakehal27 <lakehalselim27@gmail.com>
2025-10-27 00:31:13 -0400

    Add create and delete tasks for clients

--- locustfile.py
+++ locustfile.py
@@ -1,12 +1,15 @@
-from locust import HttpUser, task, between
+from locust import HttpUser, task, between, SequentialTaskSet
+from locust.exception import StopUser
 import ast
+import uuid
 
-class KeycloakUser(HttpUser):
-    wait_time = between(1,5)
+class SequentialKeycloakTasks(SequentialTaskSet):
+    client_ids = [str(uuid.uuid4()) for i in range(5)]
 
-    def on_start(self):
-        self.get_keycloak_token()
+    def regenerate_client_ids(self):
+        self.client_ids = [str(uuid.uuid4()) for i in range(5)]
 
+    @task
     def get_keycloak_token(self):
         tokenResponse = self.client.post(
             "/realms/master/protocol/openid-connect/token",
@@ -22,3 +25,35 @@ class KeycloakUser(HttpUser):
             print(f"Unable to retrieve token, recieved {tokenResponse.status_code} : {tokenResponse.text}")
             self.environment.runner.quit()
         self.client.headers["Authorization"] = "Bearer " + ast.literal_eval(tokenResponse.text)['access_token']
+        
+    @task
+    def create_clients(self):
+        self.regenerate_client_ids()
+        for id in self.client_ids:
+            clientResponse = self.client.post("/admin/realms/master/clients", json= {
+                "id": id
+            })
+
+            if clientResponse.status_code != 201:
+                print(f"Unable to create new client, recieved {clientResponse.status_code} : {clientResponse.text}")
+                raise StopUser()
+            # print(f"User {id} created!")
+    
+    @task
+    def delete_clients(self):
+        for id in self.client_ids:
+            clientResponse = self.client.delete(f"/admin/realms/master/clients/{id}")
+
+            if clientResponse.status_code != 204:
+                print(f"Unable to delete client {id}, recieved {clientResponse.status_code} : {clientResponse.text}")
+                raise StopUser()
+            # print(f"User {id} deleted!")
+    
+    @task
+    def done(self):
+        raise StopUser()
+
+
+class KeycloakUser(HttpUser):
+    wait_time = between(1,5)
+    tasks = [SequentialKeycloakTasks]
